2832c0b5c01d6b15fdc2b6fb27a0e1ca5f263e7d
Max Ward <ward.maximiliano@gmail.com>
2023-09-26 19:51:05 -0300

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Max Ward on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
